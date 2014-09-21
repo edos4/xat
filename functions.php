5a6489a0602be45bf2e6eb708138c1bbf9bf059a
Edwin Richbald A. Salinas <edos@hackermail.com>
2014-09-21 17:50:54 +0800

    fixed login and register bug

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@
 		if(get_magic_quotes_gpc())
 			$input = stripslashes($input);
 
-		$input = mysql_real_escape_string($input);
 		$input=htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 		return $input;
 	}
